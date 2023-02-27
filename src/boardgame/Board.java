f7f05f45b6740e99597682600ec36b1915fadeee
Danilo Henrique <danilomhenrique311@gmail.com>
2023-02-27 15:57:52 -0300

    Colocando primeiras peças no tabuleiro

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -38,4 +38,10 @@ public class Board {
 	public Piece piece(Position position) {
 		return pieces[position.getRow()][position.getColumn()];
 	}
+	
+	public void placePiece(Piece piece, Position position) {
+		// esta sendo atribuido a essa posição a peça que veio como argumento
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+	}
 }
